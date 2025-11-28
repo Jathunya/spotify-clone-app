a5162f4aeb97d229fa673e8bd81673d4ef65f08c
Oranut Suphantamat <Oranut_s@kkumail.com>
2025-11-29 03:48:54 +0700

    fist commit

---
+++ src/components/Display.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import DisplayHome from './DisplayHome'
+
+const Display = () => {
+  return (
+    <div className='w-full m-2 pt-4 px-4 rounded bg-[#121212] text-white over lg:w-[75%]'>
+        <Routes>
+            <Route path='/'
+            element={<DisplayHome />} />
+        </Routes>
+    </div>
+  )
+}
+
+export default Display
