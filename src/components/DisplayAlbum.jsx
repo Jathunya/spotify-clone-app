ae5361d0db026c439ac73c31e27333f1fad15834
Oranut Suphantamat <Oranut_s@kkumail.com>
2025-12-25 00:21:10 +0700

    create song data page

--- src/components/DisplayAlbum.jsx
+++ src/components/DisplayAlbum.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './Navbar'
 import { useParams } from 'react-router-dom'
 import { albumsData, assets } from '../assets/assets'
+import { songsData } from '../assets/assets'
 
 const DisplayAlbum = () => {
 
@@ -26,6 +27,28 @@ const DisplayAlbum = () => {
                 </p>
             </div>
         </div>
+        <div className='grid grid-cols-3 sm:grid-cols-4 mt-10 mb-4 pl-2 text-[#a7a7a7]'>
+          <p><b className='mr-4'>#</b>Title</p>
+          <p>Album</p>
+          <p className='hidden sm:block'>Data Added</p>
+          <img className='m-auto w-4' src={assets.clock_icon} alt=''/>
+        </div>
+        <hr />
+        {
+          songsData.map((item,index)=>(
+            <div className='grid grid-cols-3 sm:grid-cols-4 gap-2 p-2 items-center text-[#a7a7a7] hover:bg-[#ffffff] cursor-pointer'>
+              <p className='text-white'>
+                <b className='mr-4 text-[#a7a7a7]'>{index+1}
+                  <img className='inline w-10 mr-5' src={item.image} alt=''/>
+                  {item.name}
+                </b>
+              </p>
+              <p className='text-[15px]'>{albumData.name}</p>
+              <p className='text-[15px] hidden sm:block'>5 days ago</p>
+              <p className='text-[15px] text-center'>{item.duration}</p>
+            </div>
+          ))
+        }
     </>
   )
 }
